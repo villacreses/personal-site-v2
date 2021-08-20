277943a99383f65005ff779906b91080276aded9
Mario Villacreses <MarioVillacreses@outlook.com>
2021-08-20 14:03:52 -0400

    Remove Section component and reorganized corresponding styles

--- components/SectionHeader.tsx
+++ components/SectionHeader.tsx
@@ -1,8 +1,20 @@
-import { FC } from 'react';
+import { FC, HTMLProps } from 'react';
 import styles from './SectionHeader.module.scss';
 
-const SectionHeader: FC = ({ children }) => (
-  <h2 className={styles.SectionHeader}>{children}</h2>
+interface SectionHeaderProps extends HTMLProps<HTMLElement> {
+  titleStyle?: 'small'
+}
+
+const SectionHeader: FC<SectionHeaderProps> = ({ 
+  titleStyle,
+  children
+}) => (
+  <h2
+    className={styles.SectionHeader}
+    data-titleStyle={titleStyle}
+  >
+    {children}
+  </h2>
 );
 
 export default SectionHeader;
--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,3 @@
 export { default as ActionButton } from './ActionButton';
-export { default as Section } from './Section';
 export { default as SectionHeader } from './SectionHeader';
 export { default as TabList } from './TabList';
