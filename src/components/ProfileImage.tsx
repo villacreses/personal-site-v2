80cfda0fbcce8659dc59b5b915baa174862ad290
Mario Villacreses <MarioVillacreses@outlook.com>
2021-09-26 04:08:59 -0400

    Fix screen issues with image highlight & side panel responsive size

--- src/components/ProfileImage.tsx
+++ src/components/ProfileImage.tsx
@@ -4,28 +4,35 @@ import styles from './ProfileImage.module.scss';
 
 type ProfileImageProps = ComponentProps<typeof Image>;
 
+/**
+ * I'm not happy with this mess of nested divs, but I wanted
+ * to use CSS grid to at least keep index.tsx relatively div-free.
+ */
 const ProfileImage: FC<ProfileImageProps> = ({
   width,
   height,
   alt = 'Profile image',
   ...props
 }) => (
-  <div
-    className={styles.container}
-    data-container="image"
-    style={{
-      maxWidth: width,
-      maxHeight: height
-    }}
-  >
-    <Image
-      width={width}
-      height={height}
-      alt={alt}
-      {...props}
-    />
+  <div className={styles.grid}>
+    <div className={styles.ratio}>
+      <div
+        className={styles.container}
+        data-container="image"
+        style={{
+          maxWidth: width,
+          maxHeight: height
+        }}
+      >
+        <Image
+          width={width}
+          height={height}
+          alt={alt}
+          {...props}
+        />
+      </div>
+    </div>
   </div>
 );
-
 
 export default ProfileImage;
