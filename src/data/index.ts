f5967183cab2043674b823cb8042952a61b1e4e6
Mario Villacreses <MarioVillacreses@outlook.com>
2021-10-09 22:10:16 -0400

    Set up meta tags

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,6 +1,13 @@
 import exp from './Experience.content.yaml';
 import idx from './index.content.yaml';
-import { TabContent, indexContentType } from '@types';
+import meta from './index.metadata.yaml';
+
+import {
+  TabContent,
+  indexContentType,
+  metadataType
+} from '@types';
 
 export const experience = exp as Array<TabContent>;
 export const indexContent = idx as indexContentType;
+export const metadata = meta as metadataType;
