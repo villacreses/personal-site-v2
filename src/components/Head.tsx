5a4d79d60548747fcf036cbcbc58a23e9cf269c4
Mario Villacreses <MarioVillacreses@outlook.com>
2021-12-22 16:08:23 -0500

    Update meta tags to include more data for search engines

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -30,17 +30,23 @@ const Head: FC<HeadProps> = ({
       <html lang="en" />
 
       <title>{seo.title}</title>
+      <meta name="description" content={seo.description} />
 
+      {/* Google / Search Engine tags */}
       <meta name="robots" content="all" />
       <meta name="google" content="nositelinkssearchbox" />
-      <meta name="description" content={seo.description} />
+      <meta itemProp="name" content={seo.title} />
+      <meta itemProp="description" content={seo.description} />
+      <meta itemProp="image" content={seo.image} />
       
+      {/* Facebook tags */}
       <meta property="og:title" content={seo.title} />
       <meta property="og:description" content={seo.description} />
       <meta property="og:image" content={seo.image} />
       <meta property="og:url" content={seo.url} />
       <meta property="og:type" content="website" />
 
+      {/* Twitter tags */}
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:title" content={seo.title} />
       <meta property="twitter:description" content={seo.description} />
