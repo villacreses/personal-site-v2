201fe710561122e4cfb033189b357d304bc0672d
Mario Villacreses <MarioVillacreses@outlook.com>
2022-10-10 00:30:25 -0400

    Update description above timeline to production-ready copy

--- src/pages/timeline.tsx
+++ src/pages/timeline.tsx
@@ -10,17 +10,17 @@ const CareerTimeline = () => (
     <section className={styles["top-section"]}>
       <h1>{`Mario's Career History`}</h1>   
       <p>
-        <strong>Under construction! </strong>
-        This is the future home for the story of my career. Until 
-        everything is ready, please enjoy the filler text!
+        This is the timeline of my professional accomplishments, and events 
+        that have impacted my career. I hope that this will motivate you to 
+        follow your dreams and to write down your own accomplishments along 
+        the way!
       </p>
-      <p>Current to-do list:</p>
-      <ol>
-        <li>Make the timeline mobile-friendly</li>
-        <li>Replace filler text with job history</li>
-        <li>{`Implement "slider filter"`}</li>
-        <li>Add supplementary entries</li>
-      </ol>
+      <p>
+      <strong>Under construction! </strong>
+        Currently this timeline only has my work history, but I intend to add 
+        a plethora of other accomplishments and experiences that have shaped 
+        me as a professional.
+      </p>
     </section>
     <Timeline entries={timelineContent} />
   </Layout>
