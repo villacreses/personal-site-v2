985313ddb9fd6f0dfc4b32a5b31d6b45676b91c5
Mario Villacreses <MarioVillacreses@outlook.com>
2022-10-11 04:42:53 -0400

    Create useSmoothScroll hook

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,12 @@ import '@fortawesome/fontawesome-svg-core/styles.css';
 
 import { config } from '@fortawesome/fontawesome-svg-core';
 import type { AppProps } from 'next/app';
+import { useSmoothScroll } from '@hooks';
 
 config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useSmoothScroll();
   return <Component {...pageProps} />
 }
 export default MyApp
