f1d9fc747140059a1a7b670fc3c82e8b406a7d5b
Mario Villacreses <MarioVillacreses@outlook.com>
2022-10-23 20:36:03 -0400

    Reformat Experience.content.yaml

--- types/components.types.ts
+++ types/components.types.ts
@@ -66,3 +66,32 @@ export type ProjectGridItemProps = {
   website?: string;
   note?: string;
 }
+
+export enum ExperienceCategory {
+  JOBS = "JOBS",
+  HACKATHON = "HACKATHON",
+  EDUCATION = "EDUCATION",
+}
+
+/**
+ * NOTE: In the case of `endDate`, we make use of all three possible 
+ * types: `string`, `null`, and `undefined`. A null end date means the 
+ * listed position is ongoing (corresponding to an end date of "Present"),
+ * while and undefined end date signals that the listed position was a 
+ * single-day occurence (thus using an end date makes no sense)
+ */
+export type ExperienceMetadata = {
+  slug: string;
+  category: ExperienceCategory;
+  shortLabel: string;
+  companyName: string;
+  companyUrl: string;
+  positions: Array<{
+    title: string;
+    location: string;
+    startDate: string;
+    endDate?: string | null;
+    flavorText?: string;
+    impact: string[];
+  }>;
+}
